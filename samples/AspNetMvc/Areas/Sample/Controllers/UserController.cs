f9c8386aa31ed107d56cae5b6041b99bf2b5e51c
Takaaki Suzuki <xin9le@live.jp>
2021-09-27 16:58:14 +0900

    Add sample areas

---
+++ samples/AspNetMvc/Areas/Sample/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace AspNetMvc.Areas.Sample.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: Sample/User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Info()
+        {
+            return View();
+        }
+    }
+}
---
+++ samples/AspNetMvc/Areas/Sample/SampleAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace AspNetMvc.Areas.Sample
+{
+    public class SampleAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Sample";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Sample_default",
+                "Sample/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
