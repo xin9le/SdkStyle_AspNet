003283c28edb1919fdab52546c7c35e9e13f246b
Takaaki Suzuki <xin9le@live.jp>
2021-09-27 16:58:03 +0900

    Modify bundle path

--- samples/AspNetMvc/App_Start/BundleConfig.cs
+++ samples/AspNetMvc/App_Start/BundleConfig.cs
@@ -10,7 +10,7 @@ namespace AspNetMvc
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Assets/Scripts/jquery.validate*"));
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Assets/Scripts/modernizr-*"));
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Assets/Scripts/bootstrap.js"));
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Assets/Styles/bootstrap.css", "~/Assets/Styles/site.css"));
+            bundles.Add(new StyleBundle("~/Assets/Styles/css").Include("~/Assets/Styles/bootstrap.css", "~/Assets/Styles/site.css"));
         }
     }
 }
