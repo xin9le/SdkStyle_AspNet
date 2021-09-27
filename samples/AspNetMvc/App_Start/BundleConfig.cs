f753261d97dd1419e148c33492d08bf629fd6ab4
Takaaki Suzuki <xin9le@live.jp>
2021-09-27 16:49:21 +0900

    Move to assets folder

--- samples/AspNetMvc/App_Start/BundleConfig.cs
+++ samples/AspNetMvc/App_Start/BundleConfig.cs
@@ -6,11 +6,11 @@ namespace AspNetMvc
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*"));
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*"));
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css", "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Assets/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Assets/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Assets/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Assets/Scripts/bootstrap.js"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Assets/Styles/bootstrap.css", "~/Assets/Styles/site.css"));
         }
     }
 }
